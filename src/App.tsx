b1e05d3e01ab0bc6d2ff5b9349db82dd218948d7
hgmogi <hgmogi540@gmail.com>
2021-07-11 02:54:25 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 function App() {
-  return <div className="App">aaa</div>;
+  return <div className="App">aaaa</div>;
 }
 
 export default App;
