d30109f9609bdbace74aae3300d7469e3407faa8
hgmogi <hgmogi540@gmail.com>
2021-07-11 01:39:13 +0900

    initial setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAR1HXvyEqt_wv4uy9eEdMCJ35OD0Kah50",
+  authDomain: "hgentry.firebaseapp.com",
+  projectId: "hgentry",
+  storageBucket: "hgentry.appspot.com",
+  messagingSenderId: "962579619586",
+  appId: "1:962579619586:web:b22e12113fd75c442cac1b",
+  measurementId: "G-5T3M4J5EX8"
+};
+firebase.initializeApp(firebaseConfig);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      aaa
     </div>
   );
 }
