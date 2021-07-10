2e50ffaac159ad6fa7e310048486ce7c746b2138
hgmogi <hgmogi540@gmail.com>
2021-07-11 02:16:37 +0900

    initial setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import firebase from "firebase/app";
 
 const firebaseConfig = {
@@ -9,16 +9,12 @@ const firebaseConfig = {
   storageBucket: "hgentry.appspot.com",
   messagingSenderId: "962579619586",
   appId: "1:962579619586:web:b22e12113fd75c442cac1b",
-  measurementId: "G-5T3M4J5EX8"
+  measurementId: "G-5T3M4J5EX8",
 };
 firebase.initializeApp(firebaseConfig);
 
 function App() {
-  return (
-    <div className="App">
-      aaa
-    </div>
-  );
+  return <div className="App">aaa</div>;
 }
 
 export default App;
